57870181acd76d54f3f49b70f8a6fb0a177658ea
Luis Henrique <luish7572@gmail.com>
2024-10-29 22:07:46 -0300

    exercicio tsc

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+function saudacao(nome) {
+    return 'ola ' + nome;
+}
+console.log(multiplicacao(5, 3));
+console.log(saudacao('Henrique'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2
+}
+
+function saudacao(nome: string): string {
+    return 'ola ' + nome;
+}
+
+console.log(multiplicacao(5, 3));
+console.log(saudacao('Henrique'));
